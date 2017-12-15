d021b763a1ae9e7287e14881ff970b6643772f2f
Pierrot <1111079+pierrot10@users.noreply.github.com>
2017-12-15 21:26:03 +0100

    Update

--- sevo.ino
+++ sevo.ino
@@ -1,110 +1,101 @@
-/*
-* This is a simple code to test the product https://www.adafruit.com/product/169
-*/
-
-/*#if defined(__AVR__) || defined(ARDUINO_SAMD_ZERO)
-  #include <avr/pgmspace.h>
-  #include <Arduino.h>
-#elif defined(ARDUINO_ARCH_ESP8266)
-  #include <ESP.h>
-#elif defined(__MKL26Z64__)
-  #include <Arduino.h>
-#else
-  #error Unknown architecture in aes.cpp
-#endif
-*/
-
-#include <SPI.h>
-#include <Wire.h>
+#include <Arduino.h>
 #include <Servo.h>
 
 Servo servo_x;
 Servo servo_y;
 
-#define SX 9
-#define SY 10 
+//#define SX 10
+//#define SY 9 
+
+#define SX A2
+#define SY A1
 
 void setup() {
-
+ 
+  //while (!Serial);
   Serial.begin(9600);
-
+  
   delay(2000);
+  #if defined(__AVR__) || defined(ARDUINO_SAMD_ZERO)
+    Serial.println(F("AVR || SAMD_ZERO"));
+  #elif defined(ARDUINO_ARCH_ESP8266)
+    Serial.println(F("ARCH_ESP8266"));
+  #elif defined(__MKL26Z64__)
+    Serial.println(F("MKL26Z64"));
+  #else
+    #error Unknown architecture in aes.cpp
+  #endif
   Serial.println(F("Start"));
 
   pinMode(SX, OUTPUT);
-  //pinMode(SY, OUTPUT);
+  pinMode(SY, OUTPUT);
   digitalWrite(SX,LOW);
-    
-  /*
-  * Position "0" (1.5ms pulse) is middle, "90" (~2ms pulse) is all the way to the right, "-90" (~1ms pulse) is all the way to the left.
-  */
-  servo_x.attach(SX,1000,2000); // Pin, min value (0°) and max value (180°) of the micro servo (Param 2 and 3 in microsecond)
-  // servo.attach(SY);
-
-  int pos=0;
-
-
- /*
-  * Test 1
-  * Use of digitalWrite() // Values in ms
-  */
-  Serial.println("Test servo on X Axe");
-  digitalWrite(SX,HIGH);
-  delay(1);     // Move to 0°
-  digitalWrite(SX,LOW);
-  delay(1000); // Delay to see the sequence
-
-  digitalWrite(SX,HIGH);
-  delay(1.5); // MOve to 90°
-  digitalWrite(SX,LOW);
-  delay(1000);  // Delayto see the sequence
   
-  digitalWrite(SX,HIGH);
-  delay(2); // Move to 180°
-  digitalWrite(SX,LOW);
+  servo_x.attach(SX,650,2400); // pin, min and max microseconds
+  servo_y.attach(SY,850,2550);
+  
+   int pos=0;
 
-  Serial.println("Test 1 end");
-
-/*
- * Test 2
- * with the servo library
- */
-
-  Serial.println("Test 2 servo on X Axe");
-  for (pos = 0; pos <= 90; pos++) { // goes from 0 degrees to 90 degrees
+  servo_x.write(90);
+  delay(15);
+  servo_y.write(90);
+  delay(15);
+  
+  Serial.println("Test servo X 0° to 180°");
+  for (pos = 90; pos <= 180; pos++) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     Serial.println(pos);
     servo_x.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(250);                       // waits 200ms for the servo to reach the position and to see the sequence
+    delay(10);                       // waits 15ms for the servo to reach the position
   }
-  Serial.println("Test servo X Down");
-  for (pos = 90; pos >= 0; pos--) { // goes from 0 degrees to 180 degrees
+  Serial.println("Test servo X 180° to 0°");
+  for (pos = 180; pos >= 0; pos--) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     Serial.println(pos);
     servo_x.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(250);                       // waits to mark the sequence
+    delay(10);                       // waits 15ms for the servo to reach the position
   }
 
-  Serial.println(F("End test 2"));
-
-  /*
- * Solution 3
- */
+  Serial.println("Test servo X 180° to 0°");
+  for (pos = 0; pos <= 90; pos++) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Serial.println(pos);
+    servo_x.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(10);                       // waits 15ms for the servo to reach the position
+  }
   
-  servo_x.write(0); // Move to 0°
-  delay(500);
-  servo_x.write(90);  // Move to 90°
-  delay(500);
-  servo_x.write(180); // Move to 180°
-  delay(500);   
+  servo_x.write(90);
+  delay(15);
+  servo_y.write(90);
+  delay(15);
   
-}
-
-void fservo(float angle){
-  digitalWrite(SX,HIGH);
-  delay(angle);
-  digitalWrite(SX,LOW);
-}
+  Serial.println("Test servo Y 0° to 180°");
+  for (pos = 90; pos <= 180; pos++) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Serial.println(pos);
+    servo_y.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  Serial.println("Test servo Y 180° to 0°");
+  for (pos = 180; pos >= 0; pos--) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Serial.println(pos);
+    servo_y.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(10);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 0; pos <= 90; pos++) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Serial.println(pos);
+    servo_y.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+ 
+  servo_x.write(90);
+  delay(15);
+  servo_y.write(90);
+  delay(15);
+  
+  }
 
 void loop() {
   
